3d7b3645cf0534b95c630ba6b15f3a4ae480e06d
Jamoliddinov Abduraxmon <abduraxmon.jamoliddinov@smartbank.uz>
2024-12-25 14:27:08 +0500

    refactor

---
+++ Vocabulary-I/Views/FlashCardControl.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCardControl.swift
+//  Vocabulary-I
+//
+//  Created by Jamoliddinov Abduraxmon on 25/12/24.
+//
+
+import SwiftUI
+
+struct FlashCardControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashCardControl()
+}
